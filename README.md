ee8705adcdcf9c290b83497370f107c1bec48573
trwade <twade@paywith.com>
2020-04-07 12:24:22 -0700

    added funcitonality to stop and start intances

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Demo Project to manage AWS EC2 Instances and snapshots
 ## About
 
 This project is a demo, and uses boto3 to manage EC2 instance snapshots
+
+Command to list, stop and start instances
+
+pipenv run python shotty/shotty.py <command> <--project=PROJECT>
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,17 +4,62 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None, help='Only instances for project')
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('project', '<no project>')
+            )))
     return
 
+@instances.command('stop')
+@click.option('--project', default=None)
+
+def stop_instances(project):
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print('Stopping {0}...'.format(i.id))
+        i.stop()
+
+@instances.command('start')
+@click.option('--project', default=None)
+
+def start_instances(project):
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print('Starting {0}...'.format(i.id))
+        i.start()
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
